6fe529eb0bbfdd6be47f382928f05f899bce5ae9
alex <astrgan@gmail.com>
2019-05-23 22:49:58 +1000

    linkName

--- src/FoxFanParser.java
+++ src/FoxFanParser.java
@@ -1,9 +1,12 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -11,11 +14,26 @@ import java.util.Properties;
 
 public class FoxFanParser {
     Document doc = null;
+    String id = "ram";
+    URL preURL;
+    String cartoonName, rusCartoonName;
+    int intCode;
+    String link_name;
 
-    public FoxFanParser(String URL) {
+    public FoxFanParser(String seasonsURL, int season) {
 
-        String id = "ram";
-        String season = URL.substring(URL.length() - 1);
+        try {
+
+            preURL = new URL(seasonsURL+season);
+            setCartoonCode(preURL.getHost());
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+
 
         Properties connInfo = new Properties();
         connInfo.put("user", "alex");
@@ -28,18 +46,18 @@ public class FoxFanParser {
 
         try(
                 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/MultGo?", connInfo);
-                PreparedStatement statement = connection.prepareStatement("INSERT INTO cartoons (name_cartoon, name_cartoon_rus, season, episode, name_episode, description, index_cartoon, id_cartoon) VALUE (?, ?, ?, ?, ?, ?, ?, ?)")
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO cartoons (name_cartoon, name_cartoon_rus, season, episode, name_episode, description, index_cartoon, id_cartoon, shot_name, link_name) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
         ){
             doc = Jsoup
-                    .connect(URL)
+                    .connect(seasonsURL+season)
                     .userAgent("Mozilla/5.0")
                     .timeout(10 * 5000)
                     .get();
 
 
-            statement.setString(1, "Rick and Morty");
-            statement.setString(2, "Рик и Морти");
-            statement.setInt(3, Integer.parseInt(season));
+            statement.setString(1, cartoonName);
+            statement.setString(2, rusCartoonName);
+            statement.setInt(3, season);
 
 
             Element table = doc.select("table").get(2); //select the first table.
@@ -54,40 +72,71 @@ public class FoxFanParser {
                 }catch (Exception e){
                     System.out.println(rows.get(i).select("td").first().text());
                     statement.setString(7, id+"_s"+season+"e"+ep);
-                    statement.setInt(8, (10000*1)+(100*Integer.parseInt(season))+ep);
-                    statement.setInt(4, ep++);
+                    statement.setInt(8, (intCode*1)+(100*season)+ep);
+                    statement.setString(9, this.id);
+                    statement.setString(10, this.link_name);
+                    statement.setInt(4, ep);
                     statement.setString(6, rows.get(i).select("td").first().text());
                     System.out.println(statement);
                     statement.executeUpdate();
+
+                    getImage(preURL, ep+(100*season));
+
+                    ep++;
                 }
 
 
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//constructor
+
+    void getImage (URL preURL, int episode){
+        try {
 
 
-/*
-            Elements smallSeason = doc.getElementsByClass("smallSeason");
+            URL URLimage = new URL("http://"+preURL.getHost()+"/seasons/" + episode + "_big.jpg");
 
-
-
-            for (Element e: smallSeason) {
-
-                Elements em = e.getElementsByTag("em");
-
-                //System.out.println(em.select("h2").text());
-
-                em.select("h2").remove();
-                System.out.println(em.text());
-
-
-                //System.out.println(e.getElementsByTag("em"));
-
-            }*/
-
-
+            InputStream in = URLimage.openStream();
+            OutputStream out = new BufferedOutputStream(new FileOutputStream("img/"+this.id+"/"+episode+".jpg"));
+            for (int b; (b = in.read()) != -1; ) {
+                out.write(b);
+            }
+            out.close();
+            in.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    void setCartoonCode(String str){
+        System.out.println(str);
+        switch (str) {
+            case "rickandmorty.cn-fan.ru":
+                this.id = "ram";
+                cartoonName = "Rick and Morty";
+                rusCartoonName = "Рик и Морти";
+                intCode = 10000;
+                link_name = "rickandmorty";
+                break;
+            case "southpark.cc-fan.tv":
+                this.id  = "sp";
+                cartoonName = "South park";
+                rusCartoonName = "Южный парк";
+                intCode = 20000;
+                link_name = "southpark";
+                break;
+            case "paradise.nf-fan.tv":
+                this.id  = "ppd";
+                cartoonName = "Paradise PD";
+                rusCartoonName = "полиция парадайз";
+                intCode = 30000;
+                link_name = "paradisepd";
+                break;
+
+        }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        for(int i=0; i<3; i++){
-            new FoxFanParser("http://rickandmorty.cn-fan.ru/season.php?id="+(i+1));
+
+
+        for(int i=0; i<22; i++){
+            new FoxFanParser("http://southpark.cc-fan.tv/season.php?id=",(i+1));
         }
+
+        for(int i=0; i<3; i++){
+            new FoxFanParser("http://rickandmorty.cn-fan.ru/season.php?id=",(i+1));
+        }
+
+        new FoxFanParser("http://paradise.nf-fan.tv/season.php?id=",1);
 
     }
 }
